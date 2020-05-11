273f6f76e66741a5faf073d28801e9fe62eba4a1
JT Skidmore <johntoddskidmore@gmail.com>
2020-05-11 19:16:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,51 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library (library)
+ 
+   emoticons = YAML.load_file(library)
+ 
+   new_library = {}
+   
+   emoticons.each do |key, value|
+     
+     if new_library[key] == nil 
+       new_library[key] = {}
+     end
+     
+     if new_library[key][:english] == nil 
+       new_library[key][:english] = value[0]
+     end
+     
+     if new_library[key][:japanese] == nil 
+       new_library[key][:japanese] = value[1]
+     end
+     
+   end
+   
+ new_library
+ 
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+
+def get_japanese_emoticon (library, emote)
+  
+  emoticons = YAML.load_file(library)
+  binding.pry
+  emoticons.each do |key, value|
+    
+    value.each do |element|
+      
+      if element == emote 
+        return emoticons[key][value][1]
+      end
+      
+    end
+    
+  end
+  
 end
 
 def get_english_meaning
